83122f01a8b66aae10d4948fc94a4ed455c6292b
codewithmo <mohammedalimomin796@gmail.com>
2020-08-04 00:01:01 +0530

    raw zomato api with startCount

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -9,17 +9,20 @@ import { map } from 'rxjs/operators';
 export class ItemsService {
   constructor(private httpService: HttpService) {}
 
-  findAll(): Observable<AxiosResponse<Item[]>> {
-    return this.httpService
-      .get(
-        'https://developers.zomato.com/api/v2.1/search?entity_id=1&entity_type=city',
-        {
-          headers: {
-            'user-key': '75a3ac821af225e8dca370efc70dd7a2',
-            Accept: 'Application/json',
-          },
+  startCount = 20;
+  rawApi: Observable<AxiosResponse<Item[]>> = this.httpService
+    .get(
+      `https://developers.zomato.com/api/v2.1/search?entity_id=1&entity_type=city&start=${this.startCount}`,
+      {
+        headers: {
+          'user-key': '75a3ac821af225e8dca370efc70dd7a2',
+          Accept: 'Aplication/json',
         },
-      )
-      .pipe(map(response => response.data));
+      },
+    )
+    .pipe(map(response => response.data));
+
+  findAll(): Observable<AxiosResponse<Item[]>> {
+    return this.rawApi;
   }
 }
