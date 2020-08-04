0cf333c4a162ce0f74ff73ad7d590e7e3459ac39
codewithmo <mohammedalimomin796@gmail.com>
2020-08-05 01:33:44 +0530

    dbcon

--- src/configdb/db-keys.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  Host: 'ec2-54-234-44-238.compute-1.amazonaws.com',
-  Database: 'd5qtf46oadhisj',
-  User: 'hduqvkusimnuem',
-  Password: '0661535f885abf5fab549e09f874e86f6637bc970be97b1a7cdf327ce632dc2c',
-};
